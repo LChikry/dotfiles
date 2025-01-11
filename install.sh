d352a236dad12a0612dfb676cd18bf034560fda0
LChikry <LChikry@gmail.com>
2025-01-11 23:19:59 +0100

    update directory name from script to bin

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# make all .sh files in the script directory executable
-chmod +x ./script/*.sh
+# make all .sh files in the bin directory executable
+chmod +x ./bin/*.sh
 
-# Loop through all .sh files in the script directory and execute them
-for script in ./script/*.sh; do
+# Loop through all .sh files in the bin directory and execute them
+for script in ./bin/*.sh; do
     if [[ -f "$script" && -x "$script" ]]; then
         echo "Executing $script..."
         "$script"
