988fc6052a0ff92c662c5498a0f4f6b039fa719a
LChikry <LChikry@gmail.com>
2025-06-04 13:34:56 +0100

    add wezterm keybindings for native text input navigation and deletion

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -17,5 +17,39 @@ config.cursor_blink_ease_out = 'Constant'
 config.default_cursor_style = "BlinkingBlock"
 config.cursor_blink_rate = 800
 
+
+config.keys = {
+	{
+		key = 'mapped:h',
+		mods = 'CMD|SHIFT',
+		action = wezterm.action.SendKey { key = 'b', mods = 'ALT'}
+	},
+	{
+		key = 'mapped:l',
+		mods = 'CMD|SHIFT',
+		action = wezterm.action.SendKey { key = 'f', mods = 'ALT' },
+	},
+	{
+		key = 'mapped:k',
+		mods = 'CMD|SHIFT',
+		action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
+	},
+		{
+		key = 'mapped:j',
+		mods = 'CMD|SHIFT',
+		action = wezterm.action.SendKey { key = 'i', mods = 'CTRL' },
+	},
+	{
+		key = 'mapped:Backspace',
+		mods = 'CMD|SHIFT',
+		action = wezterm.action.SendKey { key = 'w', mods = 'CTRL' },
+	},
+	{
+		key = 'mapped:Backspace',
+		mods = 'CMD',
+		action = wezterm.action.SendKey { key = 'u', mods = 'CTRL' },
+	}
+}
+
 config.window_close_confirmation = "NeverPrompt"
 return config
